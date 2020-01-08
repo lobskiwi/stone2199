e67e35697c4206f1ddf573290e260390e1dea5ed
lobskiwi <njlongc@163.com>
2020-01-08 10:33:02 +0800

    my first commit vue_springboot

---
+++ vue/src/views/dashboard/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="dashboard-container">
+    <a href="" target="_blank"><img style="margin: 40px"
+      src=""></a>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'dashboard',
+    data() {
+      return {}
+    },
+  }
+</script>
---
+++ vue/src/views/user/myrole.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app-container">
+    <div class="filter-container">
+      <el-form>
+        <el-form-item>
+          <el-button type="success" icon="plus" v-if="hasPerm('user:add')" @click="showCreate">添加角色
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+</script>
+<style scoped>
+  .requiredPerm {
+    color: #ff0e13;
+  }
+</style>
